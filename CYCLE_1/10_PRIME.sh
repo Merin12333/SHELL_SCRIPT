c656e5ce5dbf54c8556cda7a8f6b65b030c50bc2
MERIN BABU <128206564+Merin12333@users.noreply.github.com>
2024-05-02 10:38:54 +0530

    .

--- CYCLE_1/10_PRIME.sh
+++ CYCLE_1/10_PRIME.sh
@@ -4,7 +4,7 @@ for ((i=2; i<=50; i++)); do
     is_prime=true
     for ((j=2; j<=i/2; j++)); do
         if [ $((i % j)) -eq 0 ]; then
-            is_prime=false
+        is_prime=false
             break
         fi
     done
